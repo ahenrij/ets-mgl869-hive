4489b7de1ac811133c70df7d2c78bbb84b5ed7ad
Henri Aïdasso <ahenrij@gmail.com>
2022-10-14 00:17:23 -0400

    parsing started

--- src/lab/scripts/logs_parser.py
+++ src/lab/scripts/logs_parser.py
@@ -11,3 +11,22 @@ HIVE-26471,standalone-metastore/metastore-server/src/main/java/org/apache/hadoop
 
 Author: Henri Aidasso <ahenrij@gmail.com>
 """
+import re
+
+input_file = "src/data/input/hive-git-logs.txt"
+output_file = "src/data/output/parsed-logs.csv"
+
+with open(input_file, "r") as f:
+
+    current_bug_id = None
+    
+    for line in f:
+        # Capture new bug id
+        if "HIVE-" in line:
+            results = re.findall("HIVE-\d+", line) # should return array of 1
+
+            if len(results) == 0:
+                print("EMPY", results)
+
+            current_bug_id = results[0] 
+            print(current_bug_id)
