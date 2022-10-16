581788461ad86bdc6231571eb0656905a20fe321
Henri Aïdasso <ahenrij@gmail.com>
2022-10-16 15:36:58 -0400

    fix: get commitId from logs

--- src/lab/scripts/logs_parser.py
+++ src/lab/scripts/logs_parser.py
@@ -18,17 +18,19 @@ output_file = "src/data/output/hive-issues-files.csv"
 
 with open(input_file, "r") as f:
     issue_id = None
-    output_content = "Issue,Filename\n"
+    commit_id = None
+    output_content = "CommitId,IssueId,Filename\n"
     
     for line in f:
         line = line.strip()
         # Capture new bug id
-        if results := re.findall("HIVE-\d+", line):
-            issue_id = results[0]
+        if results := re.findall(" HIVE-\d+", line):
+            issue_id = results[0][1:] # remove whitespace
+            commit_id = line.split(" ")[0]
             continue
         # Add new line of "bugId,filename" for Java and C++ files
         if line.endswith((".java", ".cpp", ".h")):
-            output_content += f"{issue_id},{line}\n"
+            output_content += f"{commit_id},{issue_id},{line}\n"
 
     # write output file
     with open(output_file, "w") as of:
